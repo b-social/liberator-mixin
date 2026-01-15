f4badcba3994192fbefb0688d3987f6eff15d3c4
Dominic <dominic.freeston@kroo.com>
2026-01-15 11:11:13 +0000

    Switch to deploy tokens

--- project.clj
+++ project.clj
@@ -68,7 +68,9 @@
   :cljfmt {:indents ^:replace {#".*" [[:inner 0]]}}
 
   :deploy-repositories
-  {"releases" {:url "https://repo.clojars.org" :creds :gpg}}
+  {"releases" {:url "https://repo.clojars.org"
+               :username :env/clojars_username
+               :password :env/clojars_password}}
 
   :release-tasks
   [["shell" "git" "diff" "--exit-code"]
