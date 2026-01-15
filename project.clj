55907da9d37d459510c5ba93dc46b804d7270dcb
Dominic <dominic.freeston@kroo.com>
2026-01-15 11:08:19 +0000

    Make bouncycastle versions match

--- project.clj
+++ project.clj
@@ -31,6 +31,7 @@
                  [com.fasterxml.jackson.dataformat/jackson-dataformat-smile "2.17.2"]
                  
                  ;; force upgrade from 1.68 pulled in by buddy-auth to fix vulnerability
+                 [org.bouncycastle/bcpkix-jdk15on "1.69"]
                  [org.bouncycastle/bcprov-jdk15on "1.69"]]
 
   :plugins [[lein-cloverage "1.0.13"]
